a0f1898a26e8af19839fceede7dabc5f33b12c59
Ole Kristian Sunde <ole.kristian.sunde@sparebank1.no>
2020-02-07 12:43:05 +0100

    creates symlink from ~/.tmux.conf

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 brew list | grep reattach-to-user-namespace > /dev/null 2>&1 || brew install reattach-to-user-namespace
 brew list | grep tmux > /dev/null 2>&1 || brew install tmux
+
+rm -f ~/.tmux.conf
+ln -s ~/.config/tmux-config/tmux.conf ~/.tmux.conf
